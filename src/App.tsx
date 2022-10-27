75724cfb5bfcc5351707a33e40ac10b4fd6e12c0
JuanSalvi <91103822+JuanSalvi@users.noreply.github.com>
2022-10-27 01:12:47 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+
+import WINNER_COMPS from './winner-comps.json';
+
+import './App.css';
+
+const SLOTS: Array<number> = [...Array(9).keys()];
+
+const App: React.FC = () => {
+  const [player, setPlayer] = useState<'X' | 'O'>('X');
+  const [used] = useState(() => new Map());
+  const [winnerComp, setWinnerComp] = useState<Array<number>>();
+  const hasFinished = Boolean(winnerComp);
+
+  function handleSlotClick(player: string, slot: number) {
+    used.set(slot, player);
+
+    const winnerComp = WINNER_COMPS.find(
+      comp => comp.filter(slot => used.get(slot) !== player).length === 0
+    );
+
+    setPlayer(player === 'X' ? 'O' : 'X');
+
+    if (winnerComp) return setWinnerComp(winnerComp);
+    if (used.size === SLOTS.length) return setWinnerComp([]);
+  }
+
+  function handleReset() {
+    used.clear();
+    setWinnerComp(undefined);
+  }
+
+  return (
+    <>
+      <main>
+        
+        {SLOTS.map(slot => {
+          const owner = used.get(slot);
+
+          return (
+            <div
+              key={slot}
+              className={
+                winnerComp && winnerComp.includes(slot) ? 'primary' : ''
+              }
+              onClick={() =>
+                !hasFinished && !owner && handleSlotClick(player, slot)
+              }
+            >
+              {owner}
+            </div>
+          );
+        })}
+      </main>
+      {hasFinished && (
+        <button id="reset" onClick={handleReset}>
+          Reset
+        </button>
+      )}
+    </>
+  );
+};
+
+export default App;
